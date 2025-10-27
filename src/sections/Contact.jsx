5f9040d6002fa095f6b533d0981d4ac68c20609f
Ramakant Jai <ramakantjai91@gmail.com>
2025-10-27 20:14:13 +0530

    added form submission

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -9,14 +9,14 @@ const Contact = () => {
             <h1 className='text-csGreen font-bold text-2xl text-center pt-20 pb-5'>
                 LET'S CONNECT
             </h1>
-            <form action="" className='flex flex-col'>
+            <form action="https://formspree.io/f/mayroawe" method="POST" className='flex flex-col'>
               <motion.div className='flex flex-col'
               initial={{translateY: 30, opacity: 0}}
               whileInView={{translateY: 0, opacity: '100%'}}
               transition={{delay:0.6, duration: 1, ease: 'easeInOut'}}
               viewport={{once:true}}>
               <label htmlFor="name" className='text-lg font-bold text-white md:text-xl mt-5 mb-2'>Name</label>
-              <input type="text" id="name" required className='bg-white bg-opacity-10 text-white text-lg md:text-xl mb-5 py-3 focus:outline-none pl-2'/>
+              <input type="text" id="name" name="name" required className='bg-white bg-opacity-10 text-white text-lg md:text-xl mb-5 py-3 focus:outline-none pl-2'/>
               </motion.div>
               
               <motion.div className='flex flex-col' 
@@ -25,7 +25,7 @@ const Contact = () => {
               transition={{delay: 1.2, duration: 0.6, ease: 'easeInOut'}}
               viewport={{once:true}}>
                 <label htmlFor="Email" className='text-lg font-bold text-white md:text-xl mt-5 mb-2' required>Email</label>
-                <input type="email" id='email ' className='bg-white bg-opacity-10 text-white text-lg md:text-xl mb-5 py-3 focus:outline-none pl-2'/>
+                <input type="email" id='email' name='email' className='bg-white bg-opacity-10 text-white text-lg md:text-xl mb-5 py-3 focus:outline-none pl-2'/>
               </motion.div>
               
 
@@ -35,7 +35,7 @@ const Contact = () => {
               transition={{delay:1.8, duration: 0.6, ease: 'easeInOut'}}
               viewport={{once:true}}>
               <label htmlFor="message" className='text-lg font-bold text-white md:text-xl mt-5 mb-2'>Your Message</label>
-              <textarea type="message" id='message' required className='bg-white bg-opacity-10 text-white text-lg md:text-xl mb-5 py-3 h-36 focus:outline-none pl-2'/>
+              <textarea type="message" id='message' name='message' required className='bg-white bg-opacity-10 text-white text-lg md:text-xl mb-5 py-3 h-36 focus:outline-none pl-2'/>
               </motion.div>
               
 
